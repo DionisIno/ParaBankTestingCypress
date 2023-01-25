0b9403da23c3b67f37920ac9e23bdf3adb2da755
Denis Tadzhibaev <109044090+DionisIno@users.noreply.github.com>
2023-01-25 21:31:55 +0300

    Update variable.py

--- Python/pages/variable.py
+++ Python/pages/variable.py
@@ -2,3 +2,11 @@ class SuccessfulAnswer(object):
 
     def successful_answer(self):
         return "Your account was created successfully."
+    
+class UnsuccessfulAnswer(object):
+
+    def empty_field(self):
+        return f"{self} is required."
+
+    def error_field(self):
+        return "An internal error has occurred and has been logged."
